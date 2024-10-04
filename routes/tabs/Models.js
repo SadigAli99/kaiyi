54cdb6f941d9faae8c894c7b512f641eb7949a1d
Azadev <kodingo593@gmail.com>
2024-10-04 10:23:24 +0400

    added diskmount path on the models route

--- routes/tabs/Models.js
+++ routes/tabs/Models.js
@@ -6,6 +6,7 @@ const { uploadConfig, useSharp } = require("../../config/MulterC");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
+const diskMountPath = require("../../config/mountPath");
 
 router.post(
   "/modelstab",
